2fd4416593900ffc9192c8881cbc92500c4702d8
MuhammedSelam <m.selam2022@gmail.com>
2023-08-01 01:51:19 +0300

    v1

---
+++ src/day01/C01_HelloJava.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class C01_HelloJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java"); // Hello Java
+
+        System.out.print("\"Hello Java\""); // "Hello Java"
+
+
+    }
+}
